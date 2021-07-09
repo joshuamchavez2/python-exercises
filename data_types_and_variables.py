701cbdac6bc109777f13002f37a707e612bd2a39
joshuamchavez2 <joshua.m.chavez2@gmail.com>
2021-07-09 10:56:56 -0500

    finished data_type and variables problems

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -4,9 +4,10 @@
 # If price for a movie per day is 3 dollars, how much will you have to pay?
 
 def cost_calculator(number_of_days):
+    print(chr(27) + "[2J")
     return (number_of_days * 3)
 
-print("A 9 day rental would cost " + str(cost_calculator(9)) + " dollars.\n")
+print("A 9 day rental would cost " + str(cost_calculator(9)) + " dollars.\n\n")
 
 # Suppose you're working as a contractor for 3 companies: Google,
 # Amazon and Facebook, they pay you a different rate per hour.
@@ -24,9 +25,9 @@ print("10 hours at Facebook, 6 hours at Google, and 4 hours at Amazon would pay 
 # not full and the class schedule does not conflict with her current schedule.
 
 def student_enrollment():
-    print('\nis class full? Enter y/n only\n')
-    class_full = input()
-    if(class_full == 'N' or class_full=='n'):
+    print('\nIs class full? Enter y/n only\n')
+    two_items = input()
+    if(two_items == 'N' or two_items=='n'):
         print('\nDoes the class schedule does not conflict with her current schedule? Enter y/n only\n')
         conflict = input()
         if(conflict == 'N' or conflict=='n'):
@@ -34,14 +35,72 @@ def student_enrollment():
         elif(conflict =='Y' or conflict=='y'):
             print("\nStudent cannot be enrolled due to conflicting schedule\n")
         else:
-            print(chr(27) + "[2J")
+            
             print("\nPlease make a valid selection.\n")
             student_enrollment()
-    elif(class_full == 'Y' or class_full=='y'):
+    elif(two_items == 'Y' or two_items=='y'):
         print("\nStudent cannot be enroleld due to class being full\n")
     else:
-        print(chr(27) + "[2J")
+        
         print("\nPlease make a valid selection.\n")
         student_enrollment()
 
 student_enrollment()
+
+# A product offer can be applied only if people buys more than 2 items,
+# and the offer has not expired.
+# Premium members do not need to buy a specific amount of products.
+
+def two_item_check():
+
+    print('\nDid you buy more than two items? Enter y/n only.\n')
+    two_items = input()
+    if(two_items == 'N' or two_items=='n'):
+        print('\nYou cannot recieve the product offer because you need to buy 2 or more items for the product offer.\n')
+        return False
+    elif(two_items == 'Y' or two_items=='y'):
+        return True
+    else:
+        print(chr(27) + "[2J")
+        print('\nPlease enter a valid selection.\n')
+        return two_item_check()
+        
+
+def offer_expired_check():
+    
+    print('\nIs your offer expired? Enter y/n only.\n')
+    offer_expired = input()
+    if(offer_expired == 'N' or offer_expired=='n'):
+        return True
+    elif(offer_expired == 'Y' or offer_expired=='y'):
+        print('\nYou cannot recieve the product offer because your offer is expired.\n')
+        return False
+    else:
+        
+        print('\nPlease enter a valid selection.\n')
+        return offer_expired_check()
+
+def premium_members_check():
+   
+    print('\nAre you a premium member? Enter y/n only.\n')
+    premium_member = input()
+    if(premium_member=='Y' or premium_member=='y'):
+        return True
+    elif(premium_member=='N' or premium_member=='n'):
+        return False
+    else:
+        
+        print('\nPlease enter a valid selection.\n')
+        return premium_members_check()
+
+def product_offer():
+    if(premium_members_check()==True):
+        
+        print('\nYou are eligible for the product offer.\n')
+    elif(offer_expired_check() == True and two_item_check() == True):
+        
+        print('\nYou are eligible for the product offer.\n')
+
+   
+
+product_offer()
