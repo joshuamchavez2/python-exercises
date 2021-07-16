474952c4aa4b88c01c1d4102ec2321eb4d8769ad
joshuamchavez2 <joshua.m.chavez2@gmail.com>
2021-07-16 10:29:37 -0500

    fixed a global var error in python_data and added some notes to my example.py

--- example.py
+++ example.py
@@ -1,87 +1,16 @@
+#This is a scratch pad to run random code and leave notes
 
-
-
-def leading_spaces(input_value):
-    
-    cleaned_string = ""
-
-    for index, value in enumerate(input_value):
-        if value != " ":
-            for i in range(index, len(input_value)):
-                cleaned_string += input_value[i]
-            break
-        
-    return cleaned_string
-
-def reversed_string(input_value):
-    reversed_string = ""
-    for value in reversed(input_value):
-        reversed_string += value
-    return reversed_string
-
-def trailing_spaces(input_value):
-
-    flipped_string = ""
-    cleaned_string = ""
-    
-    flipped_string = reversed_string(input_value)
-    flipped_string = leading_spaces(flipped_string)
-    
-    cleaned_string = reversed_string(flipped_string)
-    return cleaned_string
-
-
-def number_as_start(input_value):
-    cleaned_string = ""
-
-    for index, value in enumerate(input_value):
-        if value not in "123456790":
-            for i in range(index, len(input_value)):
-                cleaned_string += input_value[i]
-            break
-                
-        
-    return cleaned_string
-
-def replace_spaces_with_underscore(input_value):
-    cleaned_string = ""
-    for value in input_value:
-        if value == " ":
-            cleaned_string += "_"
-        else:
-            cleaned_string += value
-            
-    return cleaned_string
-
-def special_symbos(input_value):
-    cleaned_string = ""
-
-    for value in input_value:
-        if value not in "~!@#$%^&*()+=-,.<>/?\|~;:[]{}":
-            cleaned_string += value
-    return cleaned_string
-
-def string_to_lower_case(input_value):
-    return input_value.lower()
-
-def normalize_name(input_value):
-
-    while input_value[0] in "0123456789 " or input_value[len(input_value)-1] in "0123456789 ":
-        input_value = leading_spaces(input_value)
-        input_value = trailing_spaces(input_value)
-        input_value = number_as_start(input_value)
-        input_value = special_symbos(input_value) 
-       
-    input_value = replace_spaces_with_underscore(input_value)
-    input_value = string_to_lower_case(input_value)
-    return input_value
-
-test_code = "1 2 3 $()&^$( T#%^H#%^i%^s%^ i%^&S %&(*(a v$%&AlI#%^d p#%^YTh#%^on id#%^%&en$%&TI#$^%%$&^*Fi@$%^*(er  #^#^#@"
-
-print(normalize_name(test_code))
-
-
-
-
-
-
+""" Create block comment:  Shift + Option + A  """
+""" Copy a line of code down: Shift + Option + Down """
+""" Move by word:  Option + L or R"""
+""" Move entire line upward or downward:  Option + Up or Down """
+""" Highlight by word: Option + Shift """
+""" Newline from anywhere:  Command + Enter """
+""" Insert multiple cursor on select lines:  Option + Click """
+""" Insert multiple cursors on hightlighted lines:  Shift + Alt + I"""
+""" Highlight all occurance of a word:  Command + Shift + L """
+""" Highlight next occurance of a word Command + D"""
+""" Go to bottom of the page:  Command + Down """
+""" Go to top of the page:  Command + up """
+""" Delete a line:  Shift + Command + K """
+""" Select line:  Command + L """
